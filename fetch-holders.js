e9fb09e915291e4d531f87a49e21d48f7a184898
Dan-NFTrepo <cryptozillas@protonmail.com>
2025-03-16 00:23:53 +0530

    Add backend server files

---
+++ fetch-holders.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/fetch-holders', async (req, res) => {
+  const { contractAddress, pageIndex, pageSize } = req.query;
+  const apiKey = process.env.BLOCKVISION_API_KEY;
+
+  const url = `https://api.blockvision.org/v2/monad/collection/holders?contractAddress=${contractAddress}&pageIndex=${pageIndex}&pageSize=${pageSize}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'accept': 'application/json',
+        'x-api-key': apiKey
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
